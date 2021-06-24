386d9d10fbad3920ba3e07e828c4f5af95711cf2
Kevin G. Kayisire <kaykevin1@gmail.com>
2021-06-24 16:22:11 +0200

    variables rock

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+var (
+	someString string
+	someInteger int
+)
 
 func main() {
 	fmt.Println("Hello World!")
+	
+	someString = "what it is!"
+	someInteger = 30
+
+	fmt.Println(someString)
+	fmt.Println(someInteger)
+
+	someString = "shadowing, woo!"
+	fmt.Println(someString)
+
+	someImplicitString := "what it do!"
+	someImplicitInteger := 10
+
+	fmt.Println(someImplicitString)
+	fmt.Println(someImplicitInteger)
+
+	i := 10
+	var j string = strconv.Itoa(i)
+
+	fmt.Println(j)
 }
