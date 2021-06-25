905bda4279f907e74455f79ba9b42d4933042adf
Kevin G. Kayisire <kaykevin1@gmail.com>
2021-06-25 09:55:01 +0200

    finalized with day 1

--- main.go
+++ main.go
@@ -32,4 +32,16 @@ func main() {
 	var j string = strconv.Itoa(i)
 
 	fmt.Println(j)
+
+	// Arrays
+	grades := [...]int { 97, 85, 73, 100 }
+	fmt.Println(grades)
+
+	var students [3]string
+	fmt.Println(students)
+	students[0] = "Kevin"
+	students[2] = "Gautier"
+	students[1] = "Kayisire"
+	fmt.Println(len(students))
+	fmt.Println(students)
 }
